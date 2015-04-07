7074eebadbf15a2fba511bf49a6d2c734bd85b07
Janica Lam <janica_lam@yahoo.com>
2015-04-07 21:04:54 +0800

    files outside the folder src... put in webcontent

---
+++ src/edu/webapde/dto/db/DBManager.java
@@ -0,0 +1,16 @@
+package edu.webapde.dto.db;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBManager {
+
+	public DBManager(){
+		
+	}
+	
+	
+	
+}
