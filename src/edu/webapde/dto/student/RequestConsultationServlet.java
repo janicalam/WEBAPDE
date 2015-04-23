2a171f8232166aa57396b4e00bf0d5b7718198dc
Janica Lam <janica_lam@yahoo.com>
2015-04-24 00:48:55 +0800

    push

--- src/edu/webapde/dto/student/RequestConsultationServlet.java
+++ src/edu/webapde/dto/student/RequestConsultationServlet.java
@@ -1,39 +1,76 @@
 package edu.webapde.dto.student;
 
 import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
+import edu.webapde.dto.course.Course;
+import edu.webapde.dto.profile.Profile;
+
 /**
  * Servlet implementation class RequestConsultationServlet
  */
-@WebServlet("/RequestConsultationServlet")
-public class RequestConsultationServlet extends HttpServlet {
+@WebServlet(urlPatterns =
+{ "/RequestConsultationServlet", "/gotoSched" })
+public class RequestConsultationServlet extends HttpServlet
+{
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public RequestConsultationServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public RequestConsultationServlet()
+	{
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		// TODO Auto-generated method stub
+		process(request, response);
+	}
+
+	protected void process(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException
+	{
+		int index;
+		Profile p = (Profile) request.getSession().getAttribute("profile");
+		switch (request.getServletPath())
+		{
+
+		case "/gotoSched":
+			index = Integer.parseInt(request.getParameter("index"));
+			ArrayList<Course> enrolled = (ArrayList<Course>) request
+					.getSession().getAttribute("course");
+			response.getWriter().write(enrolled.get(index).getProfessorLast() + enrolled.get(index).getProfessorFirst());
+			break;
+		}
+		request.getRequestDispatcher("StudentRequestConsultation.jsp").forward(
+				request, response);
 	}
 
 }
